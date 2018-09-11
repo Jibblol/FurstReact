1a0451632991581afb193eb73f9802c3c9072ea2
Vegard <Jibblol@gmail.com>
2018-09-11 14:57:18 +0200

    Added keylistener to tool1

--- fursthotkey/src/components/Tool1.js
+++ fursthotkey/src/components/Tool1.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
-
+import * as Mousetrap from 'mousetrap';
 
 class Tool1 extends Component {
     state = {
-        name: 'Cat in the Hat',
+        name: 'Press button S',
     };
 
     handleChange = name => event => {
         this.setState({
-            [name]: event.target.value,
+            name: 'Button pressed: ' + event.key,
         });
+        console.log(event.key);
     };
+
+    componentDidMount() {
+        Mousetrap.bind(['s'], this.handleChange);
+    }
+
+    componentWillUnmount() {
+        Mousetrap.unbind(['s'], this.handleChange);
+    }
 
     render() {
         return (
@@ -19,10 +28,11 @@ class Tool1 extends Component {
                 <div class="row">
                     <div class="col">
                         <TextField
+                            autoFocus
                             id="name"
                             label="Name"
                             value={this.state.name}
-                            onChange={this.handleChange('name')}
+                            onKeyDown={this.handleChange('name')}
                             margin="normal"
                         />
                     </div>
