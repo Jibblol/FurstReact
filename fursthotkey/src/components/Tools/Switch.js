ce1928d7c85bb8caedf51e2661b5af91b49df8e0
Vegard <Jibblol@gmail.com>
2018-09-20 10:35:00 +0200

    Change

--- fursthotkey/src/components/Tools/Switch.js
+++ fursthotkey/src/components/Tools/Switch.js
@@ -20,7 +20,6 @@ class TestSwitch extends Component {
                 checkedB: true,
             })
         }
-        // this.setState({ [name]: event.target.checked });
         console.log(event.key)
     };
 
